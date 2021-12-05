9be294fe536de4834ac855bdfc6592663b90d26f
if0balino <balino.ical@gmail.com>
2021-12-05 13:18:21 +0700

    algoritma DH dan DES

--- diffie-hellman-algorithm-with-des/app.js
+++ diffie-hellman-algorithm-with-des/app.js
@@ -6,10 +6,10 @@ const des = require('./des');
 
 console.log('dha 1')
 const algoritma1 = algoritmaSatu.dhaPertama();
-console.log(algoritma1,'\n\n');
+console.log(algoritma1,'\n');
 console.log('dha 2')
 const algoritma2 = algoritmaDua.dhaKedua()
-console.log(algoritma2, '\n\n');
+console.log(algoritma2, '\n');
 
 // fungsi untuk pilih algoritma diffie-hellman
 function pilihdha(_ygDipilih) {
@@ -17,12 +17,16 @@ function pilihdha(_ygDipilih) {
     if (_ygDipilih === algoritma1) {
         var buff1 = crypto.randomBytes(algoritma1);
         temp = buff1.toString('hex').slice(10);
+        console.log('Algoritma          : dha 1');
+        console.log(`Kunci Rahasia K    : ${algoritma1} \n`);
         return temp;
     }
     if (_ygDipilih === algoritma2) {
         for (let i = 0; i < 2; i++) {
             temp += algoritma2;
         }
+        console.log('Algoritma          : dha 2');
+        console.log(`Kunci Rahasia K    : ${algoritma2} \n`);
         return temp.slice(4);
     }
 }
@@ -38,11 +42,20 @@ const IV = buff2.toString('hex').slice(8);
 var message = "i love you like ocean love water";
 
 console.log("~~~~~IMPLEMENTASI KRIPTOGRAFI SIMETRI DARI KUNCI SESI RAHASIA YANG TELAH DI BANGKITKAN~~~~~")
-const en = des.encryptDes(message,KEY,IV);
+console.log(`\n     Plain Text : ${message}`)
+const en = des.encryptDes(message, KEY, IV);
+const de = des.decryptDes(en,KEY,IV);
 console.log("-------------------------------------------------------------------------------------------")
 console.log("|")
 console.log("| Encryption: " + en);
-const de = des.decryptDes(en,KEY,IV);
-console.log("| Decryption：" + de);
 console.log("|")
 console.log("-------------------------------------------------------------------------------------------")
+
+console.log(`\n     Chiper Text : ${en}`)
+
+console.log("-------------------------------------------------------------------------------------------")
+console.log("|")
+console.log("| Decryption: " + de);
+console.log("|")
+console.log("-------------------------------------------------------------------------------------------")
+
